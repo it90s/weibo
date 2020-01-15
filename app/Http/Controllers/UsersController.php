4c8b274cf160d762298b57a7be68ef587430b165
it90s <751048971@qq.com>
2020-01-15 07:35:25 +0000

    提前更新

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,13 @@ use Auth;
 class UsersController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+    }
+
     public function create()
     {
         return view('users.create');
